f550e966a7f27a00401c06f1f6017f5b01be128b
c0a25180 <c0a251805c@edu.teu.ac.jp>
2026-06-09 15:45:57 +0900

    練習５と６：背景を動かした

--- pygame_image.py
+++ pygame_image.py
@@ -12,17 +12,18 @@ def main():
 
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img,True,False)#第２引数で左右反転するか否か、第3引数で上下反転するか否か
+    kk_img = pg.transform.flip(kk_img,True,False)#第2引数で左右反転するか否か、第3引数で上下反転するか否か
     tmr = 0
     while True:
+        x = tmr
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
