b87943ebf4b6104f841dd68161569ce164858968
c0a25180 <c0a251805c@edu.teu.ac.jp>
2026-06-09 15:34:22 +0900

    練習３と４：工科トン画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,17 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))#9,10行目で画面を作ってる
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img,True,False)#第２引数で左右反転するか否か、第3引数で上下反転するか否か
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
